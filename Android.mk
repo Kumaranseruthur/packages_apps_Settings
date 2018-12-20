dd21a46da69762545f1567f422905137ed93544c
maxwen <maxwen@users.noreply.github.com>
2018-12-20 20:32:50 +0000

    Add custom NeoxLounge dashboard
    Change-Id: Ic0a3b09c28b61947a369a0585a2a08bacf21f2de

--- Android.mk
+++ Android.mk
@@ -19,7 +19,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_USE_AAPT2 := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_SRC_FILES += $(call all-java-files-under, ../PixysSettings/src)
+LOCAL_SRC_FILES += $(call all-java-files-under, ../NeoxLounge/src)
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     $(ANDROID_SUPPORT_DESIGN_TARGETS) \
@@ -53,7 +53,7 @@ LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
     frameworks/support/v14/preference/res \
     frameworks/support/v7/appcompat/res \
     frameworks/support/v7/recyclerview/res \
-    packages/apps/PixysSettings/res
+    packages/apps/NeoxLounge/res
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
